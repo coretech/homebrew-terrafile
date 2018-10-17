9b35934d23f851a53599848a52770c28a453ff1a
csijira <nathaniel.ritholtz@idt.net>
2018-10-17 14:06:46 -0400

    Brew formula update for terrafile version v0.4

--- Formula/terrafile.rb
+++ Formula/terrafile.rb
@@ -1,9 +1,9 @@
 class Terrafile < Formula
   desc "Systematically manage external modules from Github for use in Terraform."
   homepage "https://github.com/coretech/terrafile"
-  url "https://github.com/coretech/terrafile/releases/download/v0.3/terrafile_0.3_Darwin_x86_64.tar.gz"
-  version "0.3"
-  sha256 "0fe5f1eb38f47d8c32397cb6df1cb0a17443d6e0cceb6b9f612088382c464435"
+  url "https://github.com/coretech/terrafile/releases/download/v0.4/terrafile_0.4_Darwin_x86_64.tar.gz"
+  version "0.4"
+  sha256 "9c92ef26900b9574c596625f67fa18ddf6d53a561a3a698a75358e9d5ea300f3"
 
   def install
     bin.install "terrafile"
